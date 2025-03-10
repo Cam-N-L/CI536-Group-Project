0229abccbf7af2fe8fb9ec568d2115a36597618a
Issy <ikelly005@yahoo.com>
2025-03-10 11:19:49 +0000

    added logout functionality

---
+++ webpage/html/processLogOut.php
@@ -0,0 +1,10 @@
+<?php
+  // Enable error reporting for debugging
+  session_start();
+
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  unset($_SESSION["username"]);
+  header("Refresh:0; url=https://ik346.brighton.domains/groupProjectTests/html/signin.php");
+  ?>
