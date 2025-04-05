35da74408950d3d5ee53bbfea339d5671154835c
Issy <ikelly005@yahoo.com>
2025-04-05 16:13:02 +0100

    Add files via upload

--- webpage/js/search.js
+++ webpage/js/search.js
@@ -1,20 +1,25 @@
 function showResult(str) {
-    if (str.length==0) {
-      document.getElementById("livesearch").innerHTML ="";
-      document.getElementById("livesearch").style.border="0px";
+  if (str.length === 0) {
+      document.getElementById("livesearch").innerHTML = "";
+      document.getElementById("livesearch").style.border = "0px";
       return;
-    }
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function() {
-      if (this.readyState==4 && this.status==200) {
-        document.getElementById("livesearch").innerHTML=this.responseText;
-        document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-        document.getElementById("livesearch").style.backgroundColor=" #A5ACB2"
-      }
-    }
-    xmlhttp.open("GET","livesearch.php?q="+str,true);
-    xmlhttp.send();
   }
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("livesearch").innerHTML = this.responseText;
+          document.getElementById("livesearch").style.border = "1px solid #A5ACB2";
+          document.getElementById("livesearch").style.backgroundColor = "#A5ACB2";
+      }
+  };
+  
+  str = encodeURIComponent(str);
+  console.log(str);
+  xmlhttp.open("GET", "../src/livesearch.php?q=" + str, true);
+  xmlhttp.send();
+}
+
 
 function titleClicked(str) {
     document.getElementById("livesearchinput").value = str;
