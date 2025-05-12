2027023bf5172ffddeba4fbae0f4c0ea8f3e3763
Issy <ikelly005@yahoo.com>
2025-05-12 17:38:06 +0100

    adding new files

---
+++ webpage2/js/activity.js
@@ -0,0 +1,11 @@
+function showResult() {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("activity-section").innerHTML=this.responseText;
+        document.getElementById("activity-section").style.border="1px solid #A5ACB2";
+      }
+    }
+    xmlhttp.open("GET","../src/gatherActivity.php?",true);
+    xmlhttp.send();
+  }
