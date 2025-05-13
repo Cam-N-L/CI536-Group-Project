deac0e0fda642295059126e0419da734d5c4a9b4
Kitty McAllister <77411551+kittymcallister@users.noreply.github.com>
2025-05-13 10:30:46 +0100

    rounds rating & displays strawberries

--- webpage2/html/src/displayFeatured.php
+++ webpage2/html/src/displayFeatured.php
@@ -5,10 +5,20 @@ function display_game($g, $conn){
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
-    $display =  "<div class=\"favorite-game\" onclick=\"openGame(". $g . ")\">
-                <img src=\"game1.jpg\">
-                <h3>" . $row["Title"] . "</h3>
-                <p>Rating: " . str_repeat("⭐", round($row["Rating"])) ."</p></div>";
+
+    $roundedRating = round($row["Rating"]);
+    $starsImagePath = "../../images/stars-$roundedRating.PNG";
+
+    $display = "<div class=\"favorite-game\" onclick=\"openGame($g)\">
+                    <img src=\"game1.jpg\">
+                    <h3>{$row['Title']}</h3>
+                    <div class=\"custom-star-rating\" data-game-id=\"$g\">
+                        <div class=\"rating-label\" style=\"margin-bottom: 5px; font-weight: bold;\">Rating:</div>
+                        <img class=\"ratingDisplay\" src=\"$starsImagePath\" alt=\"Star Rating\" data-game-id=\"$g\" />
+                        <input type=\"hidden\" name=\"rating_$g\" class=\"ratingInput\" data-game-id=\"$g\" value=\"$roundedRating\" required>
+                    </div>
+                </div>";
+    
     echo $display;
 }
 
